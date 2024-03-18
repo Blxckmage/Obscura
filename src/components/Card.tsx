3699a2a1b820332ed7ac5dc8c5133a889787790b
Blxckmage <fazadoang15@gmail.com>
2024-03-18 17:14:59 +0700

    style: use next's Image

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,17 +7,15 @@ type Props = {
 
 export default ({ src, title }: Props) => (
   <div className="relative cursor-pointer">
-    <img className="rounded object-cover" src={src} />
-    {/* <Image */}
-    {/*   src={src} */}
-    {/*   className="rounded object-cover" */}
-    {/*   alt={title} */}
-    {/*   // smaller than 1200x800 */}
-    {/*   height={800} */}
-    {/*   width={1040} */}
-    {/*   // sizes="(min-width: 1040px) calc(50vw - 158px), calc(50vw - 84px)" */}
-    {/*   // fill */}
-    {/* /> */}
+    {/* <img className="rounded object-cover" src={src} /> */}
+    <Image
+      src={src}
+      className="rounded object-cover"
+      alt={title}
+      height={800}
+      width={1040}
+      sizes="(min-width: 1040px) calc(50vw - 158px), calc(50vw - 84px)"
+    />
 
     <div className="absolute bottom-0 h-24 w-full bg-gradient-to-t from-black to-transparent px-4 pt-6">
       <div className="text-lg text-white">{title}</div>
--- src/components/homepage/Community.tsx
+++ src/components/homepage/Community.tsx
@@ -27,12 +27,14 @@ const images = [
 export default function Community() {
   return (
     <>
-      <div className="mx-auto flex min-h-screen flex-col px-3">
-        <h1 className="flex items-center p-4 text-5xl font-bold">
-          <ChevronRight size={43} strokeWidth={4} />
-          <span>Community</span>
-        </h1>
-        <div className="lg:column-4 columns-3 gap-4 px-16 py-8">
+      <div className="mx-auto flex min-h-screen flex-col">
+        <div className="px-8 py-6">
+          <h1 className="flex items-center text-5xl font-bold">
+            <ChevronRight size={43} strokeWidth={4} />
+            <span>By Community</span>
+          </h1>
+        </div>
+        <div className="column-1 md:column-3 gap-4 px-16 py-8 lg:columns-4">
           {images.map((item) => (
             <div className="mb-8 overflow-hidden rounded-lg" key={item.img}>
               <Card src={item.img} title={item.title} />
@@ -43,58 +45,3 @@ export default function Community() {
     </>
   );
 }
-
-// export default function Community() {
-//   return (
-//     <div className="mx-auto flex min-h-screen flex-col px-3">
-//       <div className="h-full w-full pt-16">
-//         <h1 className="flex items-center text-5xl font-bold">
-//           <ChevronRight size={43} strokeWidth={4} />
-//           <span>Community</span>
-//         </h1>
-//         <div className="grid h-[70vh] max-h-fit auto-cols-max grid-cols-2 gap-4 p-6 lg:grid-cols-4">
-//           <div className="relative col-span-1 flex h-full">
-//             <Image
-//               src="/assets/home-2.png"
-//               alt="alt"
-//               className="w-full object-contain"
-//               quality={100}
-//               fill={true}
-//               sizes="(min-width: 1040px) calc(27vw - 67px), calc(50vw - 44px)"
-//             />
-//           </div>
-//           <div className="relative col-span-1 h-full">
-//             <Image
-//               src="/assets/home-3.png"
-//               alt="alt"
-//               className="w-full object-contain"
-//               quality={100}
-//               fill={true}
-//               sizes="(min-width: 1040px) calc(25vw - 67px), calc(50vw - 44px)"
-//             />
-//           </div>
-//           <div className="relative col-span-1 flex h-full">
-//             <Image
-//               src="/assets/home-1.png"
-//               alt="alt"
-//               className="w-full object-contain"
-//               quality={100}
-//               fill={true}
-//               sizes="(min-width: 1040px) calc(25vw - 67px), calc(50vw - 44px)"
-//             />
-//           </div>
-//           <div className="relative col-span-1 flex h-full">
-//             <Image
-//               src="/assets/home-2.png"
-//               alt="alt"
-//               className="w-full object-contain"
-//               quality={100}
-//               fill={true}
-//               sizes="(min-width: 1040px) calc(25vw - 67px), calc(50vw - 44px)"
-//             />
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
